3ef5dfcae06ccced01b658d6ab3f32ca95f9c6ab
gchwalik <89762294+gchwalik@users.noreply.github.com>
2024-06-26 14:49:27 -0700

    comments

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 SHELL := /bin/bash
 
+# currently need to delete previous image and rebuild
+# because cannot easily create uniquely-named images
+# in docker alone
 run: cleanup
 	docker build -t flask-template . && docker run -p 80:5000 --name flask-template -it flask-template
 
--- flask-template/__init__.py
+++ flask-template/__init__.py
@@ -8,7 +8,8 @@ from . import db, click
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    # ensure the instance folder exists
+
+    # if instance folder doesn't exist, create it
     try:
         os.makedirs(app.instance_path)
     except OSError:
